691748e4942e4da4ef113f5a8aaac35c87d37e0f
Brian Sweeney <bsweeney930@gmail.com>
2018-02-08 14:53:19 -0500

    removed unnecessary comments; added logic to programmatically determine temperature exceptions;

--- logic.js
+++ logic.js
@@ -29,16 +29,28 @@ function retailerPlaceOrder(entry) {
 
 /**
  * Ship to Distributor
- * @param {org.jcsdemo.com.shipToDistributor} entry
+ * @param {org.jcsdemo.com.receivedByDistributor} entry
  * @transaction
  */
 
- function shipToDistributor(entry) {
+ function receivedByDistributor(entry) {
+    var len = entry.temp.length;
     entry.order.owner = entry.owner
-    entry.order.temp = entry.temp
-    entry.order.location = entry.location
+    entry.order.temp = entry.temp[len-1]
+    entry.order.location = entry.location[len-1]
     entry.order.status = entry.status
-    entry.order.exception = entry.exception
+    entry.order.exception = false
+
+    for (var i = 0; i < len; i++){
+        if(entry.temp[i] > 35 || entry.temp[i] < 30){
+            entry.order.exception = true;
+            entry.order.exceptionTime = entry.time[i]
+            entry.order.exceptionLocation = entry.location[i]
+            entry.order.exceptionTemp = entry.temp[i]
+            break;
+        }
+    }
+    
     return getAssetRegistry('org.jcsdemo.com.Order')
         .then(function (assetRegistry) {
             return assetRegistry.update(entry.order);
@@ -47,52 +59,50 @@ function retailerPlaceOrder(entry) {
 
 /**
  * Ship to Retailer
- * @param {org.jcsdemo.com.shipToRetailer} entry
+ * @param {org.jcsdemo.com.receivedByRetailer} entry
  * @transaction
  */
 
- function shipToRetailer(entry) {
+ function receivedByRetailer(entry) {
     entry.order.owner = entry.owner
     entry.order.temp = entry.temp
     entry.order.location = entry.location
     entry.order.status = entry.status
-    entry.order.exception = entry.exception
     return getAssetRegistry('org.jcsdemo.com.Order')
         .then(function (assetRegistry) {
             return assetRegistry.update(entry.order);
         });
  }
 
-//  /**
-//  * Ship to Retailer
-//  * @param {org.jcsdemo.com.inTransitToDistributor} entry
-//  * @transaction
-//  */
+/**
+ * In transit to Distributor
+ * @param {org.jcsdemo.com.inTransitToDistributor} entry
+ * @transaction
+ */
 
-function inTransitToDistributor(entry) {
+ function inTransitToDistributor(entry) {
+ 	entry.order.temp = entry.temp
+    entry.order.location = entry.location
+    entry.order.status = entry.status
+    return getAssetRegistry('org.jcsdemo.com.Order')
+        .then(function (assetRegistry) {
+            return assetRegistry.update(entry.order);
+        });
+ }
+
+/**
+ * In transit to Retailer
+ * @param {org.jcsdemo.com.inTransitToRetailer} entry
+ * @transaction
+ */
+
+ function inTransitToRetailer(entry) {
     entry.order.temp = entry.temp
     entry.order.location = entry.location
     entry.order.status = entry.status
-    entry.order.exception = entry.exception
     return getAssetRegistry('org.jcsdemo.com.Order')
         .then(function (assetRegistry) {
             return assetRegistry.update(entry.order);
         });
  }
 
-//  /**
-//  * Ship to Retailer
-//  * @param {org.jcsdemo.com.inTransitToRetailer} entry
-//  * @transaction
-//  */
-
-function inTransitToRetailer(entry) {
-    entry.order.temp = entry.temp
-    entry.order.location = entry.location
-    entry.order.status = entry.status
-    entry.order.exception = entry.exception
-    return getAssetRegistry('org.jcsdemo.com.Order')
-        .then(function (assetRegistry) {
-            return assetRegistry.update(entry.order);
-        });
- }
